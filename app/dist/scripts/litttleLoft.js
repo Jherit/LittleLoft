22ad26a635f9e3795a734a94c0d6943f47af0d7e
John Heritage <john@johnheritage.com>
2022-02-10 00:31:52 +0000

    Hover states and animation

--- app/dist/scripts/litttleLoft.js
+++ app/dist/scripts/litttleLoft.js
@@ -6,3 +6,15 @@ signupForm.addEventListener("submit", function(event) {
     console.log(event);
     event.preventDefault();
   });
+
+// Makes the Hero image do a little jump on mouseover
+
+  const heroImage = document.getElementById('mainImage');
+  const heroContent = document.getElementsByClassName('hero')[0];
+
+  heroContent.addEventListener("mouseenter", function() {
+    heroImage.classList.add("jumpup");
+    setTimeout(function(){
+      heroImage.classList.remove("jumpup");
+   }, 2000);
+  });
